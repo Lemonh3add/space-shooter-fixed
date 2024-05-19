32c7b03ad406e6e28549bc7f4c54ae744217a87b
Mudit Jha <muditj395@gmail.com>
2024-05-19 16:22:05 -0500

    keyboard input movement. with normalized speed vectors.

--- code/main.py
+++ code/main.py
@@ -18,7 +18,7 @@ x = 100
 # imports
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-player_direction = pygame.math.Vector2(1,1)
+player_direction = pygame.math.Vector2(0,0)
 player_speed = 300
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
@@ -39,10 +39,22 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
-            print(1)
+        # if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+        #     print(1)
+        # if event.type == pygame.MOUSEMOTION:
+        #     player_rect.center = event.pos
             
-    # draw gamea
+            
+    # input
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+
+    # normalize player vector for equal movement speed when multiple direction keys are pressed. 
+    player_direction = player_direction.normalize() if player_direction else player_direction
+    player_rect.center += player_direction * player_speed * dt
+        
+    # draw game
     diplay_surface.fill('darkgray')
     for pos in star_postitions:
         diplay_surface.blit(star_surf,pos )  
@@ -53,11 +65,7 @@ while running:
     diplay_surface.blit(laser_surf,laser_rect)
         
     # player movement
-    if player_rect.bottom > WINDOW_HEIGHT or player_rect.top < 0:
-        player_direction.y *= -1
-    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction.x *= -1
-    player_rect.center += player_direction * player_speed * dt
+
     diplay_surface.blit(player_surf, player_rect)
         
     pygame.display.update()
