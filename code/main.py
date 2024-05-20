61c34241fd3756cb72da70a7f3504dbe66b7ea1c
Mudit Jha <muditj395@gmail.com>
2024-05-20 13:31:59 -0500

    scoredispay and text

--- code/main.py
+++ code/main.py
@@ -82,6 +82,14 @@ def collisions():
         if collision_sprites:
             laser.kill()
     
+def display_score():
+    current_time = pygame.time.get_ticks() // 100
+    text_surf = font.render(str(current_time),True,(240,240,240))
+    text_rect = text_surf.get_frect(midbottom = (WINDOW_WIDTH/2, WINDOW_HEIGHT-50))
+    diplay_surface.blit(text_surf,text_rect)
+    pygame.draw.rect(diplay_surface,(240,240,240),text_rect.inflate(20,10).move(0,-8), 5,10)
+    
+
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -94,6 +102,8 @@ clock = pygame.time.Clock()
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
 laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+font = pygame.font.Font(join('images', 'Oxanium-Bold.ttf'),40)
+
 
 # sprites
 all_sprites = pygame.sprite.Group()
@@ -126,10 +136,11 @@ while running:
             
     
     # draw game
-    diplay_surface.fill('darkgray')
+    diplay_surface.fill('#3a2e3f')
     all_sprites.draw(diplay_surface)    
 
     
+    display_score()
     pygame.display.update()
     
 pygame.quit()
