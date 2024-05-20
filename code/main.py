daafe62b1e9e6bfe64934b380e1bd6ecbc13e631
Mudit Jha <muditj395@gmail.com>
2024-05-20 13:00:41 -0500

    collosion with metors and lasrs

--- code/main.py
+++ code/main.py
@@ -34,7 +34,7 @@ class Player(pygame.sprite.Sprite):
             
         recent_keys = pygame.key.get_just_pressed()
         if recent_keys[pygame.K_SPACE] and self.can_shoot:
-            Laser(laser_surf,self.rect.midtop, all_sprites)
+            Laser(laser_surf,self.rect.midtop, (all_sprites, laser_sprites))
             self.can_shoot = False
             self.laser_shoot_time = pygame.time.get_ticks()
         
@@ -88,6 +88,8 @@ laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
 
 # sprites
 all_sprites = pygame.sprite.Group()
+meteor_sprites = pygame.sprite.Group()
+laser_sprites = pygame.sprite.Group()
 for i in range(20):
     Star(all_sprites, star_surf)
 player = Player(all_sprites)
@@ -107,12 +109,25 @@ while running:
             running = False
         if event.type == meteor_event:
             x,y = randint(0,WINDOW_WIDTH), randint(-200,-100)
-            Meteor(meteor_surf, (x,y), all_sprites)
+            Meteor(meteor_surf, (x,y), (all_sprites, meteor_sprites))
             
+    # update       
     all_sprites.update(dt)     
+    collision_sprites = pygame.sprite.spritecollide(player, meteor_sprites, True)
+    if collision_sprites:
+        print(collision_sprites[0])
+        
+    for laser in laser_sprites:
+        collided_sprites = pygame.sprite.spritecollide(laser, meteor_sprites, True)
+        if collision_sprites:
+            laser.kill()
+            
+    
     # draw game
     diplay_surface.fill('darkgray')
     all_sprites.draw(diplay_surface)    
+
+    
     pygame.display.update()
     
 pygame.quit()
