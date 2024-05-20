da25bc41932ac2b9ace1fb82b635ea754179b021
Mudit Jha <muditj395@gmail.com>
2024-05-20 10:51:16 -0500

    metors working with movement

--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 from typing import Any
 import pygame
 from os.path import join
-from random import randint
+from random import randint, uniform
 
 
 
@@ -53,7 +53,26 @@ class Laser(pygame.sprite.Sprite):
         self.rect = self.image.get_frect(midbottom = pos)
     def update(self,dt):
         self.rect.centery -= 400 * dt
+        if self.rect.bottom < 0:
+            self.kill()
 
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = pos)
+        self.start_time = pygame.time.get_ticks()
+        self.lifetime = 2000
+        self.direction = pygame.Vector2(uniform(-0.5,0.5),1)
+        self.speed = randint(400,500)
+        
+    def update(self, dt):
+        self.rect.center += self.direction * self.speed * dt    
+        if pygame.time.get_ticks() -self.start_time > self.lifetime:
+            self.kill()
+            
+    
+    
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -62,21 +81,17 @@ pygame.display.set_caption('Space Shooter')
 running = True
 clock = pygame.time.Clock()
 
-# surface
-surf = pygame.Surface((100,200))
-surf.fill('orange')
-x = 100
-
-all_sprites = pygame.sprite.Group()
+# import
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
+meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
+laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+
+# sprites
+all_sprites = pygame.sprite.Group()
 for i in range(20):
     Star(all_sprites, star_surf)
 player = Player(all_sprites)
 
-meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
-
-laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
-laser_rect = laser_surf.get_frect(bottomleft =  (20, WINDOW_HEIGHT - 20))
 
 # custom event - meteor event
 meteor_event = pygame.event.custom_type()
@@ -91,13 +106,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == meteor_event:
-            print('cre')
+            x,y = randint(0,WINDOW_WIDTH), randint(-200,-100)
+            Meteor(meteor_surf, (x,y), all_sprites)
             
     all_sprites.update(dt)     
     # draw game
     diplay_surface.fill('darkgray')
-    all_sprites.draw(diplay_surface)
-        
+    all_sprites.draw(diplay_surface)    
     pygame.display.update()
     
 pygame.quit()
